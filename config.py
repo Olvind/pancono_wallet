f841c4ddf63a7ca7957f3feae6e71a12debbe030
Olvind <73144382+Olvind@users.noreply.github.com>
2025-09-05 08:32:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
 DB_FILE = "users.json"
+
+# Add your Telegram user ID here
+ADMIN_ID = os.getenv("ADMIN_ID", "7194082974")  # replace with your real Telegram ID
